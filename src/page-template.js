29b028beadd3b7d180b7f0382b66c8b885a6599f
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-27 21:46:56 -0500

    fixed errors and added reamd descrition

--- src/page-template.js
+++ src/page-template.js
@@ -56,7 +56,7 @@ return `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐔</text></svg>">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../src/styles.css">
     <title>Team Profile Generator</title>
 </head>
 <body>
